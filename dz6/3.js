af676b7305c22690167aa580c90711c4b1768218
yavtuh <yavtushenko2018seriy@gmail.com>
2022-11-30 12:08:21 +0300

    Update 3.js

--- dz6/3.js
+++ dz6/3.js
@@ -1,3 +1,4 @@
-for (let i = 10; i <= 100; i+=10){
-    console.log(`${i}$ = ${i*27} грн`);
+let number =  prompt('Введи число (1-10)', '1')
+for(let i = 1; i <= number*number; i++){
+    console.log(i);
 }
