93a139117eaa2500ee2cfff707eec94276a7553d
yavtuh <yavtushenko2018seriy@gmail.com>
2022-11-30 16:30:42 +0300

    dz8

--- dz3/index.js
+++
@@ -1,8 +0,0 @@
-const bos = window.prompt('Кількість годин?');
-const seconds = Number(bos) * 3600;
-alert(`${seconds} секунд`);
-
-
-
-
-
--- dz6/1.js
+++
@@ -1,5 +0,0 @@
-console.log("test")
-
-for (let i = 20;i <= 30; i+=0.5){
-    console.log(i)
-}
