6f682972d211cb96ac7bb466dad39bd5ab1f8d72
yavtuh <yavtushenko2018seriy@gmail.com>
2022-12-04 22:07:38 +0300

    dz10

---
+++ dz10/index.js
@@ -0,0 +1,17 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert( this.step );
+        return this;
+    }
+};
+
+ladder.up().up().down().showStep(); //1
